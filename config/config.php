67d1a04424b496ed850adb3df05185182935521a
fndsz <45010248+fndsz@users.noreply.github.com>
2024-10-05 20:03:42 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,66 @@
+<?php
+# This is an auto-generated config-file!
+# Be careful, when manual editing this!
+
+date_default_timezone_set('UTC');
+define("MMDVMLOGPATH", "/var/log/mmdvm");
+define("MMDVMINIPATH", "/opt/MMDVMHost");
+define("MMDVMINIFILENAME", "MMDVM.ini");
+define("MMDVMHOSTPATH", "/opt/MMDVMHost");
+define("ENABLEXTDLOOKUP", "on");
+define("TALKERALIAS", "on");
+define("DMRIDDATPATH", "/var/lib/mmdvm/DMRIds.dat");
+define("RESOLVETGS", "on");
+define("YSFGATEWAYLOGPATH", "/var/log/mmdvm");
+define("YSFGATEWAYLOGPREFIX", "YSFGateway");
+define("YSFGATEWAYINIPATH", "/opt/YSFGateway");
+define("YSFGATEWAYINIFILENAME", "YSFGateway.ini");
+define("YSFHOSTSPATH", "/var/lib/mmdvm");
+define("YSFHOSTSFILENAME", "YSFHosts.txt");
+define("ENABLEDMRGATEWAY", "on");
+define("DMRGATEWAYLOGPATH", "/var/log/mmdvm");
+define("DMRGATEWAYLOGPREFIX", "DMRGateway");
+define("DMRGATEWAYINIPATH", "/opt/DMRGateway");
+define("DMRGATEWAYPATH", "/opt/DMRGateway");
+define("DMRGATEWAYINIFILENAME", "DMRGateway.ini");
+define("DAPNETGATEWAYLOGPATH", "/var/lib/mmdvm");
+define("DAPNETGATEWAYLOGPREFIX", "DAPNETGateway");
+define("DAPNETGATEWAYINIPATH", "/opt/DAPNETGateway");
+define("DAPNETGATEWAYPATH", "/opt/DAPNETGateway");
+define("DAPNETGATEWAYINIFILENAME", "DAPNETGateway.ini");
+define("LINKLOGPATH", "");
+define("IRCDDBGATEWAY", "");
+define("TIMEZONE", "Asia/Shanghai");
+define("LOCALE", "zh_CN");
+define("LOGO", "");
+define("DMRPLUSLOGO", "");
+define("BRANDMEISTERLOGO", "");
+define("REFRESHAFTER", "60");
+define("SHOWCPU", "on");
+define("SHOWRPTINFO", "on");
+define("SHOWLH", "on");
+define("SHOWLOCALTX", "on");
+define("TEMPERATUREHIGHLEVEL", "60");
+define("ENABLENETWORKSWITCHING", "on");
+define("ENABLEREFLECTORSWITCHING", "on");
+define("ENABLEYSFREFLECTORSWITCHING", "on");
+define("SWITCHNETWORKUSER", "");
+define("SWITCHNETWORKPW", "");
+define("VIEWLOGUSER", "");
+define("VIEWLOGPW", "");
+define("HALTUSER", "");
+define("HALTPW", "");
+define("REBOOTUSER", "");
+define("REBOOTPW", "");
+define("RESTARTUSER", "");
+define("RESTARTPW", "");
+define("REBOOTYSFGATEWAY", "");
+define("REBOOTMMDVM", "");
+define("REBOOTSYS", "");
+define("HALTSYS", "");
+define("SHOWPOWERSTATE", "on");
+define("POWERONLINEPIN", "");
+define("POWERONLINESTATE", "");
+define("SHOWQRZ", "on");
+define("RSSI", "all");
+?>
