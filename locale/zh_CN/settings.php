466945f75b41701c5432c9d031c2c463e3c7ede6
fndsz <45010248+fndsz@users.noreply.github.com>
2024-10-05 15:03:16 +0800

    Create settings.php

---
+++ locale/zh_CN/settings.php
@@ -0,0 +1,29 @@
+<?php
+define("LANG_NAME", "Chinese");
+define("LANG_LOCALE", "zh_CN");
+define("LANG", "zh_CN");
+define("LANGCODE", "zh_CN");
+define("DATATABLESTRANSLATION", '{
+	"sEmptyTable":     "暂无相关数据",
+	"sInfo":           "_START_ / _END_ 共 _TOTAL_ 条",
+	"sInfoEmpty":      " 0 / 0 共 0 条",
+	"sInfoFiltered":   "(从 _MAX_ 条记录中筛选)",
+	"sInfoPostFix":    "",
+	"sInfoThousands":  ",",
+	"sLengthMenu":     "展示 _MENU_ 条",
+	"sLoadingRecords": "加载中...",
+	"sProcessing":     "处理中...",
+	"sSearch":         "搜索:",
+	"sZeroRecords":    "未找到匹配的记录",
+	"oPaginate": {
+		"sFirst":    "首页",
+		"sLast":     "尾页",
+		"sNext":     "下一页",
+		"sPrevious": "上一页"
+	},
+	"oAria": {
+		"sSortAscending":  ": 升序排列",
+		"sSortDescending": ": 降序排列"
+	}
+}');
+?>
